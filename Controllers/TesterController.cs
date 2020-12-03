7a4f5b0c1f5beab9a9305d4b208ee550550c242d
Igor Olikh <igoro1975@gmail.com>
2020-12-03 17:49:59 +0200

    Add project files.

---
+++ Controllers/TesterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POC.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TesterController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
